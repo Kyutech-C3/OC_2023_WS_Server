ed392922be8613880e004dc7ffb654d089f38f2f
Kurichi <yuuyapp080@gmail.com>
2023-04-26 20:13:23 +0900

    [add] initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# go-template
+
+## 起動方法
+
+```sh
+docker-compose up -d --build
+```
---
+++ app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"c3-oc2023/handler"
+	"time"
+)
+
+func main() {
+	var err error
+	time.Local, err = time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		panic(err)
+	}
+
+	e := handler.NewHandler()
+	e.Logger.Fatal(e.Start(":8080"))
+}
