52fd0227684878f5afa452a13cc824eb9f1bb842
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-04-28 02:25:01 +0900

    [change] テスト用クライアントの修正

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,76 +1,148 @@
 import { useState, useRef, useEffect, ChangeEvent } from 'react'
 import './App.css'
+// import * as WebSocket from 'websocket'
 
 function App() {
+  const [websocket, setWebsocket] = useState<WebSocket>()
+  const [socketStatus, setSocketStatus] = useState<string>('')
   const [message, setMessage] = useState<string[]>([])
   const socketRef = useRef<WebSocket>()
   const [count, setCount] = useState(0)
   const [second, setSecond] = useState(0)
   const [selectFPS, setSelectFPS] = useState(1)
+  const [uid, setUid] = useState('')
 
-  // #0.WebSocket関連の処理は副作用なので、useEffect内で実装
-  useEffect(() => {
-    // #1.WebSocketオブジェクトを生成しサーバとの接続を開始
-    const websocket = new WebSocket('ws://localhost:8080/ws?test=8967124381964')
-    socketRef.current = websocket
+  const socketServerUrl = 'ws://localhost:8080/ws'
 
-    // #2.メッセージ受信時のイベントハンドラを設定
-    const onMessage = (event: MessageEvent<string>) => {
-      setMessage(prev => [...prev, event.data])
+  const onMessage = (event: MessageEvent<string>) => {
+    const broadcastData = JSON.parse(event.data)
+    if (broadcastData.type === 'init') {
+      setUid(broadcastData.body.uid)
+      console.log(uid)
+      console.log(broadcastData)
     }
-    websocket.addEventListener('message', onMessage)
+    console.log(event.data)
+    setMessage((prev) => [...prev, event.data])
+  }
+
+  const connect = (): Promise<WebSocket> => {
+    return new Promise((resolve, reject) => {
+      const socket = new WebSocket(socketServerUrl)
+      socket.onopen = () => {
+        console.log('connected')
+        setSocketStatus('connected')
+        setWebsocket(socket)
+        resolve(socket)
+      }
+      socket.onmessage = onMessage
+      socket.onclose = () => {
+        console.log('reconnecting...')
+        setSocketStatus('reconnecting...')
+        connect()
+      }
+      socket.onerror = (err: Event) => {
+        console.log('connection error:', err)
+        reject(err)
+      }
+    })
+  }
+
+  useEffect(() => {
+    connect().then((socket) => {
+      setWebsocket(socket)
+    })
 
     // #3.useEffectのクリーンアップの中で、WebSocketのクローズ処理を実行
     return () => {
-      websocket.close()
-      websocket.removeEventListener('message', onMessage)
+      if (websocket != null) {
+        websocket.close()
+        websocket.removeEventListener('message', onMessage)
+      }
     }
   }, [])
 
   useEffect(() => {
-     const intervalId = setInterval(() => {
-      socketRef.current?.send('送信メッセージ')
-      setCount(prev => prev + 1)
-    }, Math.floor(1/selectFPS*1000));
-    return () => clearInterval(intervalId);
+    const intervalId = setInterval(() => {
+      if (uid && websocket && websocket?.readyState === websocket?.OPEN) {
+        websocket.send(
+          JSON.stringify({
+            type: 'pos',
+            body: {
+              uid: uid.toString(),
+              name: uid.toString(),
+              x: count.toString(),
+              y: count.toString(),
+            },
+          })
+        )
+      }
+      setCount((prev) => prev + 1)
+    }, Math.floor((1 / selectFPS) * 1000))
+    return () => clearInterval(intervalId)
   }, [count])
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setSecond(prev => prev + 1)
-   }, 1000);
-   return () => clearInterval(intervalId);
- }, [second])
+      setSecond((prev) => prev + 1)
+    }, 1000)
+    return () => clearInterval(intervalId)
+  }, [second])
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleChangeFPS = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setCount(0)
     setSecond(0)
+    setMessage([])
     setSelectFPS(Number(e.target.value))
   }
 
   return (
     <>
-      <div className='h-60 overflow-y-scroll'>
+      <div className="text-xl">{socketStatus}</div>
+      <div className="h-40 overflow-y-scroll bg-gray-100 px-5 py-2 rounded-xl my-5">
+        {message}
       </div>
-      <div>{count} 回</div>
-      <div>{Math.floor(count/second)} fps</div>
-      <div>
-        <select onChange={(e) => handleChange(e)}>
-          <option value={0}>1</option>
-          <option value={10}>10</option>
-          <option value={20}>20</option>
-          <option value={30}>30</option>
-        </select>
+      <div className="bg-gray-100 px-5 py-2 rounded-xl my-5">
+        <div>{count} 回</div>
+        <div>{Math.floor(count / second)} fps</div>
       </div>
-      <button
-        type="button"
-        onClick={() => {
-          setCount(0)
-          setSecond(0)
-        }}
-      >
-        リセット
-      </button>
+      <div className="bg-gray-100 px-5 py-2 rounded-xl my-5">
+        <div className="my-3 flex w-fit mx-auto">
+          <p>select fps: </p>
+          <select
+            className="bg-inherit rounded-md mx-2"
+            onChange={(e) => handleChangeFPS(e)}
+          >
+            <option value={1}>1</option>
+            <option value={10}>10</option>
+            <option value={20}>20</option>
+            <option value={30}>30</option>
+          </select>
+        </div>
+        <div className="my-3">
+          <button
+            className="rounded-md bg-sky-500 text-white px-5 py-1"
+            type="button"
+            onClick={() => {
+              setCount(0)
+              setSecond(0)
+              setMessage([])
+            }}
+          >
+            リセット
+          </button>
+        </div>
+        <div className="my-3">
+          <button
+            className="rounded-md bg-teal-500 text-white px-5 py-1"
+            type="button"
+            onClick={() => {
+              websocket?.close()
+            }}
+          >
+            再接続
+          </button>
+        </div>
+      </div>
     </>
   )
 }
